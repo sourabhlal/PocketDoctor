48b7e80fe017af9ccc596e9b6161d6b2a448296f
Sourabh <sourabhlal@yahoo.co.in>
2016-10-16 09:05:14 +0200

    adding twilio stuff

--- doctorbot.py
+++ doctorbot.py
@@ -4,6 +4,7 @@ import json
 
 import requests
 from flask import Flask, request
+from twilio import twiml
 
 app = Flask(__name__)
 
@@ -14,6 +15,17 @@ def hello_world():
 def log(message):  # simple wrapper for logging to stdout on heroku
     print str(message)
     sys.stdout.flush()
+
+@app.route("/sms", methods=['GET', 'POST'])
+def sms_reply():
+    """Respond to incoming calls with a simple text message."""
+    # Start our TwiML response
+    resp = twiml.Response()
+
+    # Add a message
+    resp.message("The Robots are coming! Head for the hills!")
+
+    return str(resp)
 
 if __name__ == '__main__':
     app.run(debug=True)
