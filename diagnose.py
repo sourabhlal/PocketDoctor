875dced9d06a4af2f3bed832fa5b0acc61808e0f
Sourabh <sourabhlal@yahoo.co.in>
2016-10-16 10:55:42 +0200

    adding infermedica stuff

---
+++ diagnose.py
@@ -0,0 +1,26 @@
+import os
+import sys
+import json
+import requests
+import infermedica_api
+api = infermedica_api.API(app_id='21794b8d', app_key='81f5f69f0cc9d2defaa3c722c0e905bf')
+
+def searchSymptom(symptom, sender_id):
+	if symptom == "fever":
+		return "s_98"
+	if symptom == "headache":
+		return "s_21"
+	if symptom == "knee pain":
+		return "s_581"
+
+
+def init_diagnose(symptom_id,age,gender,sender_id):
+	request = infermedica_api.Diagnosis(sex=gender, age=age)
+	request.add_symptom(symptom_id, 'present')
+	request = api.diagnosis(request)
+	return request
+
+def improve_diagnosis(request,sender_id,question_id,choice_id):
+	request.add_symptom(question_id,choice_id)
+	request = api.diagnosis(request)
+	return request
--- doctorbot.py
+++ doctorbot.py
@@ -1,10 +1,17 @@
 import os
 import sys
 import json
+import string
+import urllib
+
+import diagnose
 
 import requests
 from flask import Flask, request, session
 from twilio import twiml
+
+import infermedica_api
+api = infermedica_api.API(app_id='21794b8d', app_key='81f5f69f0cc9d2defaa3c722c0e905bf')
 
 # The session object makes use of a secret key.
 SECRET_KEY = 'a secret key'
@@ -22,15 +29,51 @@ def log(message):  # simple wrapper for logging to stdout on heroku
 @app.route("/sms", methods=['GET', 'POST'])
 def sms_reply():
 
+	# Get the message the user sent our Twilio number and user number
+    body = request.values.get('Body', None)
+    from_number = request.values.get('From')
+
 	# Increment the counter
     counter = session.get('counter', 0)
+    state = session.get('state', 0)
+    prevMessage = session.get('pm', 0)
+    age = session.get ('age', -1)
+    gender = session.get ('gender', -1) #male=0, female = 1
+    diagnosis = session.get ('diagnosis',{})
+
     counter += 1
 
-    # Save the new counter value in the session
-    session['counter'] = counter
-    from_number = request.values.get('From')
-    message = '{} has messaged {} {} times.' \
-        .format(from_number, request.values.get('To'), counter)
+    #Parse body
+    if pm == 1 || pm == 2:
+    	age = int(body)
+	elif pm ==3:
+		if body == "M":
+			gender = 0
+		else:
+			gender = 1
+
+
+    # Determine the right reply for this message
+    if state == 0:
+    	#newbie
+    	if age == -1 && gender == -1:
+    		message = "Hi! I'm DoctorBot. I can help you diagnose any medical conditions you may be facing. To get started please tell me your age"
+    		pm = 1
+    	elif age == -1:
+    		message = "What is your age?"
+    		pm = 2
+		elif gender == -1:
+			message = "What is your gender? (Please respond with either 'M' or 'F' "
+			pm = 3
+
+	#update state & save session
+	if state == 0 && age!=-1 && gender!=-1:
+		state = 1
+		session['state'] = state
+		session['age'] = age
+		session['gender'] = gender
+		message = '{}, {}, {}, {}' \
+        .format(from_number, state, gender, age)
 
 
     """Respond to incoming calls with a simple text message."""
