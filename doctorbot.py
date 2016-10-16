1b3e98e851dc4889aac5d81f23bab8a21da130a4
Sourabh <sourabhlal@yahoo.co.in>
2016-10-16 10:59:17 +0200

    debug

--- doctorbot.py
+++ doctorbot.py
@@ -20,70 +20,75 @@ app.config.from_object(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+	return 'Hello, World!'
 
 def log(message):  # simple wrapper for logging to stdout on heroku
-    print str(message)
-    sys.stdout.flush()
+	print str(message)
+	sys.stdout.flush()
 
 @app.route("/sms", methods=['GET', 'POST'])
 def sms_reply():
 
 	# Get the message the user sent our Twilio number and user number
-    body = request.values.get('Body', None)
-    from_number = request.values.get('From')
+	body = request.values.get('Body', None)
+	from_number = request.values.get('From')
+	log(body)
 
 	# Increment the counter
-    counter = session.get('counter', 0)
-    state = session.get('state', 0)
-    prevMessage = session.get('pm', 0)
-    age = session.get ('age', -1)
-    gender = session.get ('gender', -1) #male=0, female = 1
-    diagnosis = session.get ('diagnosis',{})
+	counter = session.get('counter', 0)
+	state = session.get('state', 0)
+	pm = session.get('pm', 0)
+	age = session.get ('age', -1)
+	gender = session.get ('gender', -1) #male=0, female = 1
+	diagnosis = session.get ('diagnosis',{})
 
-    counter += 1
+	counter += 1
 
-    #Parse body
-    if pm == 1 || pm == 2:
-    	age = int(body)
-	elif pm ==3:
+	#Parse body
+	if pm == 1 or pm == 2:
+		age = int(body)
+	elif pm == 3:
 		if body == "M":
 			gender = 0
 		else:
 			gender = 1
 
 
-    # Determine the right reply for this message
-    if state == 0:
-    	#newbie
-    	if age == -1 && gender == -1:
-    		message = "Hi! I'm DoctorBot. I can help you diagnose any medical conditions you may be facing. To get started please tell me your age"
-    		pm = 1
-    	elif age == -1:
-    		message = "What is your age?"
-    		pm = 2
+	# Determine the right reply for this message
+	if state == 0:
+		#newbie
+		if age == -1 and gender == -1:
+			message = "Hi! I'm DoctorBot. I can help you diagnose any medical conditions you may be facing. To get started please tell me your age"
+			pm = 1
+		elif age == -1:
+			message = "What is your age?"
+			pm = 2
 		elif gender == -1:
 			message = "What is your gender? (Please respond with either 'M' or 'F' "
 			pm = 3
+	elif state == 1:
+		message = "boom"
 
 	#update state & save session
-	if state == 0 && age!=-1 && gender!=-1:
+	if state == 0 and age!=-1 and gender!=-1:
 		state = 1
-		session['state'] = state
-		session['age'] = age
-		session['gender'] = gender
 		message = '{}, {}, {}, {}' \
-        .format(from_number, state, gender, age)
+		.format(from_number, state, gender, age)
+
+	session['pm'] = pm
+	session['state'] = state
+	session['age'] = age
+	session['gender'] = gender
 
 
-    """Respond to incoming calls with a simple text message."""
-    # Start our TwiML response
-    resp = twiml.Response()
+	"""Respond to incoming calls with a simple text message."""
+	# Start our TwiML response
+	resp = twiml.Response()
 
-    # Add a message
-    resp.message(message)
+	# Add a message
+	resp.message(message)
 
-    return str(resp)
+	return str(resp)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+	app.run(debug=True)
